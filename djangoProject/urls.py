54fb44ba58e9b2978fc6a13239da135accb6b250
Mazev <77510376+Mazev@users.noreply.github.com>
2021-06-23 00:37:04 +0300

    new

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
 
 urlpatterns = (
     path('admin/', admin.site.urls),
-    path('main_app/', include('templates.main_app.urls')),
-    path('secondary_app/', include('templates.secondary_app.urls'))
+    path('', 'main_app.urls'),
+    path('', 'secondary_app.urls')
 
 )
